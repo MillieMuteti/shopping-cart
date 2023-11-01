ab554842054c188b02e1ae6f1741b77aaa53b4cc
Millie Muteti <milliemuteti@gmail.com>
2023-11-01 15:33:58 +0300

    create context

---
+++ src/context/Context.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react'
+
+const Cart=createContext();
+
+const Context = ( { children}) => {
+  return (
+    <Cart.Provider>
+        {children}
+      
+    </Cart.Provider>
+  )
+}
+
+export default Context
